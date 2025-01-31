1769f7980805ff119c0d08345d996c1beae4c27d
Psoewish <psoewish@proton.me>
2025-01-31 22:19:55 +0100

    Add additional checks in setup script

--- archsetup.sh
+++ archsetup.sh
@@ -21,20 +21,28 @@ if ! grep -q "^ILoveCandy" /etc/pacman.conf; then
 fi
 
 # Set up the chaotic AUR
-echo 'Setting up the Chaotic AUR'
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-sudo echo "[chaotic-aur]" >> /etc/pacman.conf
-sudo echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+
+if ! grep -q "^[chaotic-aur]" /etc/pacman.conf; then
+  echo 'Setting up the Chaotic AUR'
+  sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+  sudo pacman-key --lsign-key 3056513887B78AEB
+  sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
+  sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+  sudo echo "[chaotic-aur]" >> /etc/pacman.conf
+  sudo echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+fi
 
 # Update pacman config
-sudo pacman -Sy
+sudo pacman -Sy --noconfirm
 
-# Set up Paru
-echo 'Installing Paru'
-sudo pacman -S paru --noconfirm
+# Set up Paru if not already installed
+if ! type -q 'paru'; then
+  echo 'Installing Paru'
+  sudo pacman -S paru --noconfirm
+fi
+
+# Set Paru to have a reverse order
+sudo sed -i "/BottomUp/s/^#//g" /etc/paru.conf
 
 # Update full system
 paru --noconfirm
