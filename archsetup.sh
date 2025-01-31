a70bf73d62abb6b524aceaacb0de9bd9096f565e
Psoewish <psoewish@proton.me>
2025-01-31 20:47:42 +0100

    archsetup test

---
+++ archsetup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+# Set up Pacman config
+echo -e "Setting up pacman"
+pacman_conf = "/etc/pacman.conf"
+pacman_settings = (
+  "Color"
+  "CheckSpace"
+  "VerbosePkgLists"
+  "ParallelDownloads"
+)
+
+# Uncomment settings if not already done
+for line in "${pacman_settings[@]}"; do
+  if grep -q "^#line" "$pacman_conf"; then
+    sudo sed -i "s/^#$line/$line/" "pacman_conf"
+  fi
+done
+
+# Enable "I love candy" mode
+if grep -q "^ParallelDownloads" "$pacman_conf" && ! grep -q "^ILoveCandy" "$pacman_conf"; then
+  sudo sed -i "/^nparallelndownloads/a ILoveCandy" "$pacman_conf"
+fi
+
+# Set up the chaotic AUR
+echo 'Setting up the Chaotic AUR'
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+echo "[chaotic-aur]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+
+# Update pacman config
+sudo pacman -Sy
+
+# Set up Paru
+echo 'Installing Paru'
+sudo pacman -S paru --noconfirm
+
+# Update full system
+paru --noconfirm
+
