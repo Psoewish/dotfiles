a0b09d4e3dcd5a55f04e8562e67d14bcc89732ff
Psoewish <psoewish@proton.me>
2025-03-09 06:14:36 +0100

    Initial qutebrowser setup

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,23 @@
+import rosepine
+
+config.load_autoconfig(True)
+
+rosepine.setup(c, 'rose-pine', True)
+
+c.colors.webpage.darkmode.enabled = True
+c.auto_save.session = True
+c.scrolling.bar = 'never'
+c.scrolling.smooth = True
+c.statusbar.show = 'in-mode'
+
+c.tabs.pinned.shrink = False
+c.tabs.pinned.frozen = True
+c.tabs.position = 'left'
+c.tabs.last_close = 'startpage'
+c.tabs.padding = {'bottom': 8, 'left': 8, 'right': 8, 'top': 8}
+c.tabs.width = 200
+
+c.fonts.default_family = 'CaskaydiaCove Nerd Font'
+c.fonts.default_size = '12pt'
+
+config.bind('<Alt+Ctrl+C>', 'config-cycle tabs.show always never')
