65d4064a5a0be6466058d47516d74e068962fc57
Psoewish <psoewish@proton.me>
2025-03-10 01:34:29 +0100

    Clean up fish plugins

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -48,4 +48,15 @@ starship init fish | source
 
 # Zoxide settings
 zoxide init fish --cmd cd | source
+
+# fzf settings
 fzf --fish | source
+function fzf --wraps="fzf"
+    set -Ux FZF_DEFAULT_OPTS "
+	    --color=fg:#908caa,bg:#191724,hl:#ebbcba
+	    --color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
+	    --color=border:#403d52,header:#31748f,gutter:#191724
+	    --color=spinner:#f6c177,info:#9ccfd8
+	    --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
+    command fzf
+end
