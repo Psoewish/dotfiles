0a0797ac1b9d8168d1d2df7fc51149b378c1e99d
psoewish <psoewish@proton.me>
2025-04-02 05:57:07 +0200

    "fix" this function

--- .config/fish/functions/tools/copycat.fish
+++ .config/fish/functions/tools/copycat.fish
@@ -1,4 +1,7 @@
 function copycat
-    cat $argv | wl-copy
+    if type -q wl-copy
+        cat $argv | wl-copy
+    else
+        echo "figure out what clipboard manager you're using and fix this function, dummy"
+    end
 end
-
