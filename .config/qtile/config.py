a86032cf327bb79b3ec29c63a94e4d717e933058
psoewish <psoewish@proton.me>
2025-04-03 02:40:42 +0200

    Fix group setup

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,6 +1,34 @@
-from libqtile import bar, layout, hook, qtile, widget
-from libqtile.config import Click, Drag, Group, Key, Match, Screen, DropDown, ScratchPad
+import subprocess
+from libqtile import bar, layout, hook, widget
+from libqtile.config import Click, Drag, Group, Key, Screen, DropDown, ScratchPad, Match
 from libqtile.lazy import lazy
+
+mod = "mod4"
+terminal = "wezterm"
+launcher = "rofi -show drun"
+browser = "qutebrowser"
+browser2 = "zen-browser"
+
+follow_mouse_focus = True
+bring_front_click = False
+floats_kept_above = True
+cursor_warp = True
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+auto_minimize = True
+widget_defaults = {
+    "font": "CaskaydiaCove Nerd Font",
+    "fontsize": 12,
+    "padding": 3,
+}
+
+
+@hook.subscribe.startup_once
+def autostart():
+    processes = [["~/.screenlayout/desktop.sh"], ["vesktop"], ["steam -silent"]]
+    for p in processes:
+        subprocess.Popen(p)
 
 
 def init_keys():
@@ -20,10 +48,11 @@ def init_keys():
         Key([mod], "m", lazy.layout.normalize()),
         Key([mod, "shift"], "Return", lazy.layout.toggle_split()),
         Key([mod], "Return", lazy.spawn(terminal)),
+        Key([mod], "B", lazy.spawn(browser)),
         Key([mod], "Tab", lazy.next_layout()),
-        Key([mod], "w", lazy.window.kill()),
+        Key([mod], "q", lazy.window.kill()),
         Key([mod], "v", lazy.window.toggle_floating()),
-        Key([mod, "control"], "r", lazy.reload_config()),
+        Key([mod, "shift"], "Escape", lazy.reload_config()),
         Key([mod, "control"], "q", lazy.shutdown()),
         Key([mod], "Space", lazy.spawn(launcher)),
         # Key([mod], "f", lazy.function(toggle_max())),
@@ -51,12 +80,25 @@ def init_mouse():
 
 
 def init_groups():
-    def _groupkeys(key, name):
-        keys.append(Key([mod], key, lazy.group[name].toscreen()))
-        keys.append(Key([mod, "shift"], key, lazy.window.togroup(name)))
-        return Group(name)
-
     groups = [Group(i) for i in "12345"]
+
+    for i in groups:
+        keys.extend(
+            [
+                Key(
+                    [mod],
+                    i.name,
+                    lazy.to_screen(0) if i.name in "1234" else lazy.to_screen(1),
+                    lazy.group[i.name].toscreen(),
+                ),
+                Key(
+                    [mod, "shift"],
+                    i.name,
+                    lazy.window.togroup(i.name, switch_group=True),
+                ),
+            ]
+        )
+
     dropdowns = [
         DropDown("term", "wezterm"),
         DropDown("yazi", "wezterm start -- yazi"),
@@ -71,14 +113,17 @@ def init_layouts():
     border = 1
 
     layouts = [
-        layout.MonadTall(border_width=border, margin=margin, single_margin=margin),
+        # layout.Columns(border_width=border, margin=margin, single_margin=margin),
+        layout.Tile(
+            border_width=border,
+            margin=margin,
+            add_after_last=True,
+            ratio=0.5,
+            master_match=[Match(wm_class="qutebrowser"), Match(wm_class="vesktop")],
+        ),
         layout.Max(border_width=border, margin=margin),
     ]
     return layouts
-
-
-def init_floating_layout():
-    return layout.Floating()
 
 
 @hook.subscribe.client_new
@@ -105,38 +150,18 @@ def init_screens():
                 24,
             ),
             wallpaper=wallpaper,
+            wallpaper_mode="fill",
         ),
-        Screen(wallpaper=wallpaper),
+        Screen(wallpaper=wallpaper, wallpaper_mode="fill"),
     ]
     return screens
 
-
-mod = "mod4"
-terminal = "wezterm"
-launcher = "rofi -show drun"
-browser = "qutebrowser"
-browser2 = "zen-browser"
-
-follow_mouse_focus = True
-bring_front_click = False
-floats_kept_above = True
-cursor_warp = True
-auto_fullscreen = True
-focus_on_window_activation = "smart"
-reconfigure_screens = True
-auto_minimize = True
-widget_defaults = {
-    "font": "CaskaydiaCove Nerd Font",
-    "fontsize": 12,
-    "padding": 3,
-}
 
 keys = init_keys()
 mouse = init_mouse()
 groups = init_groups()
 layouts = init_layouts()
 screens = init_screens()
-floating_layout = init_floating_layout()
 
 # Still not sure if I need this but I'll just keep it in lol
 wmname = "LG3D"
